bce1e73480f9ea67b0f62bf7f27a4ac559d38078
Roman Radchenko <nodenwwsfww@gmail.com>
2022-06-25 14:02:21 +0300

    fixed commit

--- app_user/admin.py
+++ app_user/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import School, Relative, Student
 
-# Register your models here.
+
+@admin.register(School)
+class SchoolAdmin(admin.ModelAdmin):
+    list_display = ['id', 'region', 'district', 'city', 'school']
+
+
+@admin.register(Relative)
+class RelativeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'last_name', 'first_name', 'patronymic', 'phone_number', 'email']
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'school', 'parent', 'user_class', 'last_name', 'first_name', 'patronymic', 'phone_number']
--- app_user/models.py
+++ app_user/models.py
@@ -1,3 +1,30 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
+class School(models.Model):
+    region = models.CharField(max_length=100, verbose_name='Регион')
+    district = models.CharField(max_length=100, verbose_name='Городской округ/Муниципальный район')
+    city = models.CharField(max_length=100, verbose_name='Населённый пункт')
+    school = models.CharField(max_length=150, verbose_name='Образовательное учреждение')
+
+    def __str__(self):
+        return self.school
+
+
+class Relative(models.Model):
+    last_name = models.CharField(max_length=25, verbose_name='Фамилия')
+    first_name = models.CharField(max_length=25, verbose_name='Имя')
+    patronymic = models.CharField(max_length=25, verbose_name='Отчество')
+    phone_number = models.CharField(max_length=11, verbose_name='Номер телефона')
+    email = models.EmailField(verbose_name='Электронная почта')
+
+
+class Student(models.Model):
+    school = models.ForeignKey(School, on_delete=models.CASCADE, verbose_name='Школа')
+    parent = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Родитель')
+    user_class = models.CharField(max_length=3, verbose_name='Класс')
+    last_name = models.CharField(max_length=25, verbose_name='Фамилия')
+    first_name = models.CharField(max_length=25, verbose_name='Имя')
+    patronymic = models.CharField(max_length=25, verbose_name='Отчество')
+    phone_number = models.CharField(max_length=11, verbose_name='Номер телефона')
