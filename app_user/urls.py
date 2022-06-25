896bf0d0ddf58e7e65e379ef8ef92aae020df555
Roman Radchenko <nodenwwsfww@gmail.com>
2022-06-25 10:39:46 +0300

    Remove django files

--- app_user/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import AnotherLogin, AnotherLogout, register_view
-
-urlpatterns = [
-    path('login/', AnotherLogin.as_view(), name='login'),
-    path('register/', register_view, name='register'),
-    path('logout/', AnotherLogout.as_view(), name='logout'),
-]
